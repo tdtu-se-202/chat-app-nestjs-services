a1f3e8561eb689061791c609da39e318b4327c22
Kienn Thiett <smoothkt4951@gmail.com>
2023-07-18 18:41:00 +0700

    breaking: delete src

--- src/database/migrations.ts
+++
@@ -1,3 +0,0 @@
-import { init1678617916226 } from "./migrations/1678617916226-init";
-
-export const migrations = [init1678617916226]
--- src/helpter/auth-helper.ts
+++
@@ -1,9 +0,0 @@
-import jwt_decode from 'jwt-decode';
-
-export class AuthHelper {
-  static getEmailFromToken(token: string) {
-    const payload: Record<string, any> = jwt_decode(token);
-
-    return payload.email;
-  }
-}
