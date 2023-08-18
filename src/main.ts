828be54bb3e995d6e776a84893fbf2c3a62f1581
Kienn Thiett <smoothkt4951@gmail.com>
2023-08-18 15:04:47 +0700

    chore: fix line separator and allow origins test

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   SwaggerModule.setup("api", app, document);
 
   app.enableCors({
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3001"],
     credentials: true,
   });
 
